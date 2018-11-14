b5d6b1faf1a1f125b87f12a177d0a5a544899ded
hugo-mtn7 <hugo.mtn7@gmail.com>
2018-11-14 17:17:32 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div className="App">
+      <Navbar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+  import { Link } from "react-router-dom";
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink>Xubaka</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Gallerie</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Technologie</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Qui sommes nous ?</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          <NavbarBrand href="/">Sodium Cycle</NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
+}
