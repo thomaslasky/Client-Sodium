4b6b78a046e7ebb0f8f316f0820d9f776321e82a
hugo-mtn7 <hugo.mtn7@gmail.com>
2018-11-14 17:37:48 +0100

    Carousel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import Carousel from './components/Carousel/Carousel'
 
 class App extends Component {
   render() {
     return ( 
       <div className="App">
       <Navbar />
+      <Carousel />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
