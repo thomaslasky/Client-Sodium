8ada5f9be830d9e5e3cd6d490dffda741c691eac
thomaslasky <35836004+thomaslasky@users.noreply.github.com>
2018-11-20 17:10:47 +0100

    Modif nav minim

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,60 +1,62 @@
 import React from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
- } from 'reactstrap';
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+} from 'reactstrap';
 
 export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-        /*TODO space content on the bar*/
-      <div>
-        <Navbar color="dark" dark expand="md">
-          <NavbarToggler onClick={this.toggle} />
-          <NavbarBrand className="d-block d-sm-none" href="/">Sodium Cycle</NavbarBrand>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="mr-auto w-100 justify-content-around " navbar>
-              <NavItem>
-                <NavLink>Xubaka</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>Gallerie</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>Technologie</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>Qui sommes nous ?</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>Précommande</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>Contact</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-          <NavbarBrand className="d-none d-sm-block" href="/">Sodium Cycle</NavbarBrand>
-        </Navbar>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	
+	toggle() {
+		this.setState(
+			{isOpen: !this.state.isOpen}
+		);
+	}
+	
+	render() {
+		return (
+			/*TODO space content on the bar*/
+			<div>
+				<Navbar color="dark" dark expand="md">
+					<NavbarToggler onClick={this.toggle} />
+					<NavbarBrand className="d-block d-sm-none" href="/">Sodium Cycle</NavbarBrand>
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="mr-auto w-100 justify-content-around " navbar>
+							<NavItem>
+								<NavLink>Xubaka</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Gallerie</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Technologie</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Qui sommes nous ?</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Précommande</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Contact</NavLink>
+							</NavItem>
+						</Nav>
+					</Collapse>
+					<NavbarBrand className="d-none d-sm-block" href="/">Sodium Cycle</NavbarBrand>
+				</Navbar>
+			</div>
+		);
+	}
 }
