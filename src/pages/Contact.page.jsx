6c0137d1140654a45da782a02b2d0fb6f1507180
thomaslasky <35836004+thomaslasky@users.noreply.github.com>
2018-11-21 14:06:37 +0100

    add contact

--- src/pages/Contact.page.jsx
+++ src/pages/Contact.page.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
-
+import {
+	Button,
+	Form,
+	FormGroup,
+	Input,
+	InputGroup,
+	InputGroupAddon,
+} from 'reactstrap';
+import cssClasses from "./Contact.module.css";
 
 function ContactPage() {
-	return (
+	return <div className="col-md-6 m-auto">
 		<div>
-			<InputGroup>
-				<InputGroupAddon addonType="prepend">@</InputGroupAddon>
-				<Input placeholder="email" />
-			</InputGroup>
+			<h1>Nous contacter</h1>
 		</div>
-	);
+		<Form className="mb-2">
+			<FormGroup>
+				<InputGroup>
+					<InputGroupAddon addonType="prepend">@</InputGroupAddon>
+					<Input placeholder="email" type="email" />
+				</InputGroup>
+			</FormGroup>
+			<FormGroup>
+				<InputGroup>
+					<InputGroupAddon addonType="prepend">Téléphone</InputGroupAddon>
+					<Input placeholder="téléphone" type="tel" />
+				</InputGroup>
+			</FormGroup>
+			<FormGroup>
+				<InputGroup>
+					<InputGroupAddon addonType="prepend">Sujet</InputGroupAddon>
+					<Input placeholder="sujet" type="text" />
+				</InputGroup>
+			</FormGroup>
+			<FormGroup>
+				<InputGroupAddon addonType="prepend" className={cssClasses.urMessage}>Votre Message</InputGroupAddon>
+				<Input type="textarea" id="message" />
+			</FormGroup>
+			<Button className="m-auto d-block">Submit</Button>
+		</Form>
+	</div>;
 }
 
 export default ContactPage;
