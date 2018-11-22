54bd24c658fd3131f9815da97fb8a53b5e435abf
thomaslasky <35836004+thomaslasky@users.noreply.github.com>
2018-11-22 14:44:17 +0100

    404 end

--- src/pages/Page404.page.jsx
+++ src/pages/Page404.page.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Img from 'react-image';
+
+import pic404 from '../assets/Images/Background/404notfound.jpg';
 
 //import cssModule from 'Page404.module.css';
 
 function notFound() {
 	return (
 		<div>
-			<h1>Not Found</h1>
+			<div>
+				<div className="w-100">
+					<Img class="img-fluid" src={pic404} />
+				</div>
+			</div>
 		</div>
 	);
 }
