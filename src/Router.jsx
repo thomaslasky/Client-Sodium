f623820bc0bf3508354749095cb8f135ed090fc5
hugo-mtn7 <hugo.mtn7@gmail.com>
2018-11-22 15:41:57 +0100

    fix router Switch

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -11,17 +11,20 @@ import Who from "./pages/Qui.page";
 import Page404 from "./pages/Page404.page";
 
 export default () => (
-	<Router>
-		<div>
-			<Layout top={<Navbar />} bottom={<Footer />} container={
-				<div>
-					<Route path="/" exact component={Home} />
-					<Route path="/contact" exact component={Contact} />
-					<Route path="/who-are-we" exact component={Who} />
-          <Route path="*" component={Page404} />
-				</div>
-			}
-			/>
-		</div>
-	</Router>
+  <Router>
+    <div>
+      <Layout
+        top={<Navbar />}
+        bottom={<Footer />}
+        container={
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/who-are-we" exact component={Who} />
+            <Route path="*" component={Page404} />
+          </Switch>
+        }
+      />
+    </div>
+  </Router>
 );
