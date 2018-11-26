eb63e8d42b53e4d721e099b50662e2d0297c4190
thomaslasky <35836004+thomaslasky@users.noreply.github.com>
2018-11-26 09:28:15 +0100

    Send Contact Axios

--- src/pages/Contact.page.jsx
+++ src/pages/Contact.page.jsx
@@ -10,51 +10,63 @@ import {
 import cssClasses from "./Contact.module.css";
 import axios from "axios";
 
-function ContactPage() {
+export default function ContactPage() {
+	
+	function handleContactSubmit(credentials) {
+		
+		let contact = new URLSearchParams();
+		contact.append('objet', credentials.objet);
+		contact.append('email', credentials.email);
+		contact.append('message', credentials.message);
+		contact.append('telephone', credentials.telephone);
+		
+		return (
+			axios.post("http://cours/TestReact/mail.php", contact, {
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				}
+			})
+		);
+	}
 	
 	return <div className="col-md-6 m-auto">
 		<div className="mt-3">
 			<h1 className="text-center">Nous contacter</h1>
 		</div>
-		<Form className="mb-2" id="formulaireContact">
+		<Form className="mb-2"
+		      onSubmit={e => {
+			      e.preventDefault();
+			      const email = e.target.elements.email.value;
+			      const telephone = e.target.elements.tel.value;
+			      const objet = e.target.elements.objet.value;
+			      const message = e.target.elements.message.value;
+			
+			      handleContactSubmit({email, telephone, objet, message});
+		      }}
+		>
 			<FormGroup>
 				<InputGroup>
 					<InputGroupAddon addonType="prepend">@</InputGroupAddon>
-					<Input placeholder="email" type="email" />
+					<Input placeholder="email" type="email" name="email" />
 				</InputGroup>
 			</FormGroup>
 			<FormGroup>
 				<InputGroup>
 					<InputGroupAddon addonType="prepend">Téléphone</InputGroupAddon>
-					<Input placeholder="téléphone (facultatif)" type="tel" />
+					<Input placeholder="téléphone (facultatif)" type="tel" name="tel" />
 				</InputGroup>
 			</FormGroup>
 			<FormGroup>
 				<InputGroup>
 					<InputGroupAddon addonType="prepend">Objet</InputGroupAddon>
-					<Input placeholder="objet" type="text" />
+					<Input placeholder="objet" type="text" name="objet" />
 				</InputGroup>
 			</FormGroup>
 			<FormGroup>
 				<InputGroupAddon addonType="prepend" className={cssClasses.urMessage}>Votre Message</InputGroupAddon>
-				<Input type="textarea" id="message" rows="10" />
+				<Input type="textarea" id="message" rows="10" name="message" />
 			</FormGroup>
-			<Button className="m-auto d-block" onClick={
-				() => {
-					axios.post('', {
-						     firstname: 'thomas',
-						     lastname : 'tartas',
-					     })
-					     .then(function (response) {
-						     console.log(response);
-					     })
-					     .catch(function (error) {
-						     console.log(error);
-					     });
-				}
-			}>Submit</Button>
+			<Button className="m-auto d-block">Submit</Button>
 		</Form>
 	</div>;
 }
-
-export default ContactPage;
