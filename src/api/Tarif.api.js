122ec7fa0f214e83bab1eee51f636509d2149723
thomaslasky <35836004+thomaslasky@users.noreply.github.com>
2018-12-03 15:16:19 +0100

    Add File API

--- src/api/Tarif.api.js
+++ src/api/Tarif.api.js
@@ -1,13 +1,5 @@
 import axios from "axios";
-
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function flatReq(req, ms = 500) {
-    return Promise.all([sleep(ms), req])
-                  .then(([_, res]) => res.data.resultats);
-}
+import {flatReq} from './Api.manager';
 
 export function getElems() {
     return flatReq(axios.get(""));
