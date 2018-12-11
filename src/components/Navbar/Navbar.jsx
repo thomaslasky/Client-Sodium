ede71d13f4cae5bf8f5a286e54b3623bb8a219e7
Hugo <hugo.mtn7@gmail.com>
2018-12-11 10:04:37 +0100

    with text navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,78 +1,114 @@
 import React from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem
 } from "reactstrap";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import withText from "../../withText.hoc";
 
-export default class extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        
-        this.toggle = this.toggle.bind(this);
-        this.closeNavbar = this.closeNavbar.bind(this);
-        this.state = {
-            isOpen: false
-        };
+class NavbarXubaka extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.closeNavbar = this.closeNavbar.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  closeNavbar() {
+    if (this.state.isOpen === true) {
+      this.toggle();
     }
-    
-    toggle() {
-        this.setState(
-            {isOpen: !this.state.isOpen}
-        );
-    }
-    
-    closeNavbar() {
-        if (this.state.isOpen === true) {
-            this.toggle();
-        }
-    }
-    
-    render() {
-        return (
-            <div>
-                <Navbar color="dark" dark expand="md">
-                    <NavbarToggler onClick={this.toggle} />
-                    <NavbarBrand className="d-block d-md-none" href="/">Sodium Cycle</NavbarBrand>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="mr-auto w-100 justify-content-around " navbar>
-                            <NavItem>
-                                <Link to="/" onClick={this.closeNavbar} className="nav-link">Xubaka</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link to="/Gallery" onClick={this.closeNavbar}
-                                      className="nav-link">Galerie</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link to="/technology" onClick={this.closeNavbar}
-                                      className="nav-link">Technologie</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link to="/who-are-we" onClick={this.closeNavbar}
-                                      className="nav-link">Qui Sommes-Nous ?</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link to="/tarif" onClick={this.closeNavbar}
-                                      className="nav-link">Tarif</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link to="/preorder" onClick={this.closeNavbar}
-                                      className="nav-link">Précommander</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link to="/contact" onClick={this.closeNavbar}
-                                      className="nav-link">Contact</Link>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                    <NavbarBrand className="d-none d-md-block" href="/">Sodium Cycle</NavbarBrand>
-                </Navbar>
-            </div>
-        );
-    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <NavbarBrand className="d-block d-md-none" href="/">
+            Sodium Cycle
+          </NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto w-100 justify-content-around " navbar>
+              <NavItem>
+                <Link to="/" onClick={this.closeNavbar} className="nav-link">
+                  {this.props.t("textNav1")}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to="/Gallery"
+                  onClick={this.closeNavbar}
+                  className="nav-link"
+                >
+                  {this.props.t("textNav2")}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to="/technology"
+                  onClick={this.closeNavbar}
+                  className="nav-link"
+                >
+                  {this.props.t("textNav3")}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to="/who-are-we"
+                  onClick={this.closeNavbar}
+                  className="nav-link"
+                >
+                  {this.props.t("textNav4")}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to="/tarif"
+                  onClick={this.closeNavbar}
+                  className="nav-link"
+                >
+                  {this.props.t("textNav5")}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to="/preorder"
+                  onClick={this.closeNavbar}
+                  className="nav-link"
+                >
+                  {this.props.t("textNav6")}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to="/contact"
+                  onClick={this.closeNavbar}
+                  className="nav-link"
+                >
+                  {this.props.t("textNav7")}
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          <NavbarBrand className="d-none d-md-block" href="/">
+            {this.props.t("textNavBrand")}
+          </NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
 }
+
+export default withText(NavbarXubaka);
